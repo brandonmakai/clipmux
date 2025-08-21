fbc7eee34fe0fe4160015c2dbb8112995e777a34
Brandon Williams <brandonmakaiw@gmail.com>
2025-08-21 18:52:21 -0400

    Feat: Add Clipboard Operations
    Add clipboard operations to handle RobotGo functionality.
    Define interfaces to allow swapping in future clipboard
    implementations for better performance tracking and testability.

---
+++ internal/strategy.go
@@ -0,0 +1,31 @@
+package internal 
+
+import (
+	"github.com/go-vgo/robotgo"
+)
+
+type ReadPaster interface { 
+	Reader  
+	Paster
+}
+
+type Reader interface {
+	Read() (string, error)
+}
+
+type Paster interface {
+	Paste(text string) error
+}
+
+type RobotGo struct {}
+
+func (r RobotGo) Read() (string, error) {
+	text, err := robotgo.ReadAll()
+	return text, err 
+}
+
+func (p RobotGo) Paste(text string) error {
+	err := robotgo.PasteStr(text)
+	return err
+}
+
