aedc56cf2446cd96e42eccf81e51e1661b746a75
Brandon Williams <brandonmakaiw@gmail.com>
2025-08-29 21:09:40 -0400

    feat: Add Daily Logger Rotation

--- internal/logger/logger_ops.go
+++ internal/logger/logger_ops.go
@@ -2,8 +2,8 @@ package logger
 
 import (
 	"fmt"
-	"log"
 	"os"
+	"path/filepath"
 	"time"
 )
 
@@ -11,11 +11,40 @@ func newLogger(file *os.File, toStdout bool) *Logger {
 	return &Logger{file: file, toStdout: toStdout}
 }
 
+func (l *Logger) rotateIfNeeded() error {
+    today := time.Now().Format("2006-01-02")
+    if today == l.currentDay && l.file != nil {
+        return nil 
+    }
+
+		filename := l.file.Name()
+		dir := filepath.Dir(filename)
+		ext := filepath.Ext(filename)
+
+		newFile := today + ext 
+    newFile = filepath.Join(dir, newFile) 
+    f, err := os.OpenFile(newFile, os.O_CREATE|os.O_APPEND|os.O_WRONLY, 0644)
+    if err != nil {
+        return err
+    }
+
+    l.file = f
+    l.currentDay = today
+    return nil
+}
+
 func (l *Logger) Info(msg string) {
 	l.mu.Lock()
 	defer l.mu.Unlock()
+	if err := l.rotateIfNeeded(); err != nil {
+		panic(err)
+	}
+
 	line := fmt.Sprintf("%s [INFO] %s\n", time.Now().Format(time.RFC3339), msg)
-	fmt.Fprintf(l.file, line) 
+ 	if _, err := fmt.Fprint(l.file, line); err != nil {
+			fmt.Fprintf(os.Stderr, "Failed to write to log file: %v\n", err)
+	}
+
 
 	if l.toStdout {
 		fmt.Println(line)
@@ -25,11 +54,18 @@ func (l *Logger) Info(msg string) {
 func (l *Logger) Error(msg string) {
 	l.mu.Lock()
 	defer l.mu.Unlock()
+	if err := l.rotateIfNeeded(); err != nil {
+		panic(err)
+	}
+
 	line := fmt.Sprintf("%s [FATAL] %s\n", time.Now().Format(time.RFC3339), msg) 
-	fmt.Fprintf(l.file,line) 
-	log.Fatal(msg)
+	if _, err := fmt.Fprint(l.file, line); err != nil {
+			fmt.Fprintf(os.Stderr, "Failed to write to log file: %v\n", err)
+	}
 
 	if l.toStdout {
-		fmt.Println(line)
+			fmt.Print(line)
 	}
+
+	os.Exit(1)
 }
